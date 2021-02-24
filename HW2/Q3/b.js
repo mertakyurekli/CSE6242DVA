59a81053bac3ea9ac928b0964d6d745b2efc3ad8
iyutpo <y15653862659@outlook.com>
2021-02-24 14:25:57 -0500

    2/24

---
+++ HW2/Q3/b.js
@@ -0,0 +1,193 @@
+///////////////////////////////////// Q3.b ///////////////////////////////////////
+    // append the svg obgect to the body of the page
+    // appends a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg2 = d3.select("body").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    d3.csv("boardgame_ratings.csv").then(function(data) {
+        console.log(data);
+        // format the data
+        data.forEach(function (d) {
+            d.date = parseTime(d.date);
+            d["Catan=count"] = +d["Catan=count"];
+            d['Catan=rank'] = +d['Catan=rank'];
+            d["Dominion=count"] = +d["Dominion=count"];
+            d["Dominion=rank"] = +d["Dominion=rank"];
+            d["Codenames=count"] = +d["Codenames=count"];
+            d["Codenames=rank"] = +d["Codenames=rank"];
+            d["Terraforming Mars=count"] = +d["Terraforming Mars=count"];
+            d["Terraforming Mars=rank"] = +d["Terraforming Mars=rank"];
+            d["Gloomhaven=count"] = +d["Gloomhaven=count"];
+            d["Gloomhaven=rank"] = +d["Gloomhaven=rank"];
+            d["Magic: The Gathering=count"] = +d["Magic: The Gathering=count"];
+            d["Magic: The Gathering=rank"] = +d["Magic: The Gathering=rank"];
+            d["Dixit=count"] = +d["Dixit=count"];
+            d["Dixit=rank"] = +d["Dixit=rank"];
+            d["Monopoly=count"] = +d["Monopoly=count"];
+            d["Monopoly=rank"] = +d["Monopoly=rank"];
+        });
+
+        // Scale the range of the data
+        x.domain(d3.extent(data, function (d) {
+            return d.date;
+        }));
+        y.domain([0, d3.max(data, function (d) {
+            return Math.max(d["Catan=count"], d["Dominion=count"],
+                d["Codenames=count"], d["Terraforming Mars=count"],
+                d["Gloomhaven=count"], d['Magic: The Gathering=count'],
+                d['Dixit=count'], d['Monopoly=count']);
+        })]);
+
+        // generate color for each line:
+        for (let i = 0; i < colorArray[0].length; i++) {
+            var lineDict = {};
+            lineDict.color = colorScheme(i);
+            lineArray.push(lineDict);
+        }
+
+        //////////////////////////////////////// Start Setting Axis //////////////////////////////////////////
+        svg2.append('text')
+            .attr('x', 150)
+            .attr('y', 0)
+            .text('Number of Ratings 2016-2020 with Rankings');
+
+        // Add the X axis:
+        svg2.append('g')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(d3.axisBottom(x)
+                .tickFormat(d3.timeFormat("%b %y")))    // https://stackoverflow.com/questions/40173533/customize-the-d3-month-or-year-tick-format/40175517
+        svg2.append('text')
+            .attr('class', 'x label')
+            .attr('x', 275)
+            .attr('y', height + 40)
+            .text('Month');
+
+        // Add the Y Axis
+        svg2.append("g")
+            .call(d3.axisLeft(y));
+        svg2.append('text')
+            .attr('transform', 'rotate(270)')
+            .attr('class', 'y label')
+            .attr('x', -250)
+            .attr('y', -50)
+            .text('Num of Ratings');
+        //////////////////////////////////////// End Setting Axis //////////////////////////////////////////
+
+        // Add valueline1 to 'path' where 'valueline1' represents 'Catan'. So on and so forth:
+        for (let i = 0; i < categories.length; i++) {
+            var subCategories = ['Catan', 'Codenames', 'Terraforming Mars', 'Gloomhaven'];
+            var lineFunction = multiLines(categories[i]);
+            svg2.append("path")
+                .data([data])
+                .attr("class", "line")
+                .style('stroke', lineArray[i].color)
+                .style('fill', 'none')                // Remove shaded area
+                .attr("d", lineFunction);
+            if (subCategories.includes(categories[i])) {      // Add dots/circles to each line
+                svg2.selectAll('myCircles')        // Add data label to circles
+                    .data(data)
+                    .enter()
+                    .append('circle')               // A better way to concatenate the following '.attr's can be found here: https://stackoverflow.com/questions/20822466/how-to-set-multiple-attributes-with-one-value-function
+                    .attr('fill', function (d, j) {
+                        if (j % 3 === 2) {
+                            return lineArray[i].color;
+                        }
+                    })
+                    .attr('stroke', function (d, j) {
+                        if (j % 3 === 2) {
+                            return 'none';
+                        }
+                    })
+                    .attr('cx', function (d, j) {
+                        if (j % 3 === 2) {
+                            return x(d.date);
+                        }
+                    })
+                    .attr('cy', function (d, j) {
+                        if (j % 3 === 2) {
+                            return y(d[categories[i] + '=count']);
+                        }
+                    })
+                    .attr('r', function (d, j) {
+                        if (j % 3 === 2) {
+                            return 10;
+                        }
+                    })
+
+                svg2.selectAll('myCircles')
+                    .data(data)
+                    .enter()
+                    .insert('text')
+                    .text(function (d, j) {          // Don't use 'i' as index here because the outer for loop is using 'i' as index already
+                        if (j % 3 === 2) {
+                            return d[categories[i] + '=rank'] + j;
+                        }
+                    })
+                    .attr("x", function (d, j) {
+                        if (j % 3 === 2) {
+                            return x(d.date) - 5;
+                        }
+                    })
+                    .attr("y", function (d, j) {
+                        if (j % 3 === 2) {
+                            return y(d[categories[i] + '=count']) + 3;
+                        }
+                    })
+                    .attr('font-size', '8px')
+                    .attr('stroke', 'white')
+                    .attr('font', 'sans-serif')
+            }
+        }
+        ;
+
+        // Use a for loop to add labels for each category:
+        for (let i = 0; i < categories.length; i++) {
+            svg2.append('text')
+                .attr('transform', 'translate(' + (width + 3) + ',' + y(data[data.length - 1][categories[i] + '=count']) + ')')
+                .attr('font-size', '10px')
+                .attr('text-anchor', 'start')
+                .style('fill', lineArray[i].color)
+                .text(categories[i]);
+        }
+
+        // Finally, we'll add a legend to represent numbers in circles:
+        var legend_keys = ["BoardGameGeek Rank"];
+        var circleLegend = svg2.selectAll('.circleLegend')
+            .data(legend_keys)
+            .enter()
+            .append('g')
+            .attr('class', 'circleLegend')
+            .attr('font-size', '12px')
+            .attr("transform", function (d, i) {
+                return "translate(" + (width) + "," + height + ")";
+            });
+        circleLegend.append("text")
+            .text(function (d) {
+                return d;
+            })
+            .attr("transform", "translate(15,9)"); //align texts with boxes
+        svg2.selectAll('.circleLegendIcon')        // Add data label to circles
+            .data(legend_keys)
+            .enter()
+            .append('circle')               // A better way to concatenate the following '.attr's can be found here: https://stackoverflow.com/questions/20822466/how-to-set-multiple-attributes-with-one-value-function
+            .attr('fill', 'black')
+            .attr('stroke', 'none')
+            .attr('cx', width + margin.right - 120)
+            .attr('cy', height - 20)
+            .attr('r', 15)
+        svg2.selectAll('.circleLegendIcon')
+            .data(legend_keys)
+            .enter()
+            .insert('text')
+            .text('rank')
+            .attr('stroke', 'white')
+            .attr("x", width + margin.right - 130)
+            .attr("y", height - 18)
+            .attr('font-size', '12px')
+            .attr('font-weight', 'lighter')
+    });
